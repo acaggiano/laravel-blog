5e5f6991464578633b0a62d922f249ed69244c85
Anthony Caggiano <onaiggaca@gmail.com>
2017-05-19 12:08:27 -0400

    changed header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,8 +29,8 @@
 
     <div class="blog-header">
       <div class="container">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        <h1 class="blog-title">Anthony's Sick Ass Blog</h1>
+        <p class="lead blog-description">An example blog template built with Bootstrap and Laravel.</p>
       </div>
     </div>
 
